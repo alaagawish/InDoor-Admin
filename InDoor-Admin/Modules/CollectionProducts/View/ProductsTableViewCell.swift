e25ea4dcd39c2891967b46952a588869ffaf82eb
Ahmed Abdo <AhmdMoAbdo@gmail.com>
2023-06-08 20:11:42 +0300

    add product table view cell

---
+++ InDoor-Admin/Modules/CollectionProducts/View/ProductsTableViewCell.swift
@@ -0,0 +1,47 @@
+//
+//  ProductsTableViewCell.swift
+//  InDoor-Admin
+//
+//  Created by Ahmed on 08/06/2023.
+//
+
+import UIKit
+
+class ProductsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productPrice: UILabel!
+    @IBOutlet weak var productDescription: UILabel!
+    @IBOutlet weak var productName: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.shadowRadius = 5.0
+        layer.shadowColor = UIColor.gray.cgColor
+        layer.shadowOffset = CGSize(width: 3.3, height: 5.7)
+        layer.shadowOpacity = 0.7
+        layer.masksToBounds = false
+        contentView.layer.cornerRadius = 20
+        contentView.layer.masksToBounds = true
+        backgroundColor = .clear
+        contentView.backgroundColor = .white
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+    override var frame: CGRect{
+        get {
+            return super.frame
+        }
+        set(newFrame){
+            var frame = newFrame
+            frame.origin.x += 8
+            frame.origin.y += 8
+            frame.size.width -= 2 * 8
+            frame.size.height -= 2 * 8
+            super.frame = frame
+        }
+    }
+}
