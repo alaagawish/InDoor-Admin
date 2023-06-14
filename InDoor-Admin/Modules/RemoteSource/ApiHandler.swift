2b09c7a5b8271ae016c9725da582bf19c6d7998b
Ahmed Abdo <AhmdMoAbdo@gmail.com>
2023-06-14 15:15:23 +0300

    add update product network method

--- InDoor-Admin/Modules/RemoteSource/ApiHandler.swift
+++ InDoor-Admin/Modules/RemoteSource/ApiHandler.swift
@@ -11,10 +11,11 @@ import Alamofire
 
 protocol ApiService{
     func getData(url:String,completionHandler: @escaping(RootClass?) -> Void)
+    func updateProduct(method:HTTPMethod, url:String, parameters: Parameters)
 }
 class ApiHandler:ApiService{
     
-    func getData(url:String,completionHandler: @escaping(RootClass?) -> Void){
+    func getData(url:String, completionHandler: @escaping(RootClass?) -> Void){
         let headers:HTTPHeaders = ["X-Shopify-Access-Token": "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"]
         let url = URL(string: url)!
         AF.request(url,method: .get, headers: headers).response { response in
@@ -26,6 +27,25 @@ class ApiHandler:ApiService{
                 }catch let error{
                     print(error.localizedDescription)
                     completionHandler(nil)
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    func updateProduct(method:HTTPMethod, url:String, parameters: Parameters){
+        let headers:HTTPHeaders = ["X-Shopify-Access-Token": "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"
+        ]
+        let url = URL(string: url)!
+        AF.request(url,method: method,parameters: parameters, encoding:URLEncoding.httpBody , headers: headers).response{ response in
+            switch response.result {
+            case .success(let data):
+                do{
+                    let result = try JSONDecoder().decode(RootClass.self, from: data!)
+                    print("updated")
+                }catch let error{
+                    print(error.localizedDescription)
                 }
             case .failure(let error):
                 print(error.localizedDescription)
