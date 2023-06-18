9fd8aadfbf63610db34017ca114efe5e67e6afda
Ahmed Abdo <AhmdMoAbdo@gmail.com>
2023-06-18 04:35:37 +0300

    design discount UI

---
+++ InDoor-Admin/Modules/Discounts/View/DiscountsTableViewCell.swift
@@ -0,0 +1,51 @@
+//
+//  DiscountsTableViewCell.swift
+//  InDoor-Admin
+//
+//  Created by Ahmed on 18/06/2023.
+//
+
+import UIKit
+
+class DiscountsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var discountUsage: UILabel!
+    @IBOutlet weak var discountName: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.shadowRadius = 5.0
+        layer.shadowColor = UIColor.gray.cgColor
+        layer.shadowOffset = CGSize(width: 3.3, height: 5.7)
+        layer.shadowOpacity = 0.7
+        layer.masksToBounds = false
+        contentView.layer.cornerRadius = 20
+        contentView.layer.masksToBounds = true
+        backgroundColor = .clear
+        contentView.backgroundColor = .white    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    override var frame: CGRect{
+        get {
+            return super.frame
+        }
+        set(newFrame){
+            var frame = newFrame
+            frame.origin.x += 8
+            frame.origin.y += 8
+            frame.size.width -= 2 * 8
+            frame.size.height -= 2 * 8
+            super.frame = frame
+        }
+    }
+    
+    func setDiscountData(discount: DiscountCodes){
+        discountName.text = discount.code
+        discountUsage.text = String(discount.usageCount ?? 0)
+    }
+    
+}
